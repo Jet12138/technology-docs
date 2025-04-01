2347304c2ed9bb47f43b6c2efffbbb8d7cde9543
Jet <390715001@qq.com>
2025-04-01 22:45:02 +0800

    Updated: 加了两个问题

--- algorithm/binarytree.js
+++ algorithm/binarytree.js
@@ -1,4 +1,13 @@
+/*
+二叉查找树（Binary Search Tree），（又：二叉搜索树，二叉排序树）它或者是一棵空树，
+或者是具有下列性质的二叉树： 若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值； 
+若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值； 它的左、右子树也分别为二叉排序树。
+二叉搜索树作为一种经典的数据结构，它既有链表的快速插入与删除操作的特点，又有数组快速查找的优势；
+所以应用十分广泛，例如在文件系统和数据库系统一般会采用这种数据结构进行高效率的排序与检索操作。
+*/
+
 //创建二叉排序树（又叫，二叉搜索树，二叉排序树，其中序遍历的结果就是从小到大递增的排序结果。）
+// https://github.com/sisterAn/JavaScript-Algorithms/issues/87
 function Node(data, left, right) {
     this.data = data;
     this.left = left;
@@ -41,9 +50,10 @@ BST.prototype.insert = function (data) {
 };
 
 //二叉树先序遍历
-BST.prototype.prevOrder = function (node) {
+BST.prototype.prevOrder = function (node, result = []) {
     if (node) {
         // console.log(node.show() + '');
+        result.push(node.data);
         this.prevOrder(node.left);
         this.prevOrder(node.right);
     }
